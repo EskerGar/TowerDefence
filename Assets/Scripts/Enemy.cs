52f427e4172cf24e33a78d35d89fc4edefe9ee70
Esker_Gar <lobachevartem@gmail.com>
2020-05-12 17:04:47 +0300

    PathFinding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private ScriptableObject enemySettings;
+    public ScriptableObject ReturnSettings => enemySettings;
+    public GameObject Target { get; private set; }
+
+    private void Awake()
+    {
+        Target = GameObject.FindWithTag("Base");
+;    }
+}
--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthComponent : MonoBehaviour
 {
    [SerializeField] private float maxHealth;
-   private float _health;
+   [SerializeField]private float _health;
    public event Action OnChange;
 
-   private void ChangeHealth(float amount)
+   private void Start()
+   {
+      _health = maxHealth;
+   }
+
+   public void ChangeHealth(float amount)
    {
       _health -= amount;
       OnChange?.Invoke();
