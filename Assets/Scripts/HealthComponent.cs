bb0bf525321c4d190545c9f753d93b788a20197d
Esker_Gar <lobachevartem@gmail.com>
2020-05-12 15:25:46 +0300

    Initial commit

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+   [SerializeField] private float maxHealth;
+   private float _health;
+   public event Action OnChange;
+
+   private void ChangeHealth(float amount, GameObject who)
+   {
+      
+   }
+}
